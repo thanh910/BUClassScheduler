66ab92ac379667d2ff1dfbec48c13db38675e4d0
Thanh <107339866+thanh910@users.noreply.github.com>
2022-07-29 00:10:48 -0400

    Add files via upload

--- WebScraper.py
+++ WebScraper.py
@@ -1,61 +1,122 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jul 16 03:29:19 2022
-
-@author: thanh, grace, derek, rylie, jenny
-"""
-
 
 import requests
-from bs4 import BeautifulSoup
+import lxml.html as lh
+import pandas as pd
+
+import time
+start_time = time.time()
+
+
 
 semester = input("Enter a semester (ex. Fall 2022): ")
+semester = semester.replace(" ", "+")
+
+#numCourses = int(input("How many courses are you taking? "))
+
+#allCourses = []
+#for c in range(numCourses):
 courseName = input("Enter a course (ex. CAS XX 123): ")
 
-semester = semester.replace(" ", "+")
+
+#for c in allCourses:
 courseName = courseName.upper().split(" ")
+if len(courseName) < 4:
+    courseName += [""]
 
+#
 
-URL = "https://www.bu.edu/link/bin/uiscgi_studentlink.pl/1658458219?ModuleName=univschr.pl&SearchOption" + \
-    "Desc=Class+Number&SearchOptionCd=S&KeySem=20233&ViewSem=" + semester + \
-    "&College=" + courseName[0] + "&Dept=" + courseName[1] + "&Course=" + courseName[2] + "&Section="
+allCoursesDatabase = []
+
+#for courseName in allCourses: 
+#num of pages rerun
+pageCount = 0
+#Create empty list
+col=[]  
+
+while True:
     
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, "html.parser")
+    URL = "https://www.bu.edu/link/bin/uiscgi_studentlink.pl/1658458219?ModuleName=univschr.pl&SearchOption" + \
+            "Desc=Class+Number&SearchOptionCd=S&KeySem=20233&ViewSem=" + semester + \
+            "&College=" + courseName[0] + "&Dept=" + courseName[1] + "&Course=" + courseName[2] + "&Section=" + courseName[3]
+                    
+    courseNameString = "".join(courseName)
+    page = requests.get(URL)
+    pageContent = lh.fromstring(page.content)
+    tableElements = pageContent.xpath('//tr')
 
-for linebreak in soup.find_all('br'):
-    linebreak.replace_with("\n")
+    inputValues = pageContent.xpath('//input')
+    nextPageSearchInputs = [v.value for v in inputValues][1:5]
 
-results = soup.find_all("font")
 
-sections =[]
+    #print([len(T) for T in tableElements]) #test for printing the width of all tables in URL
 
-for line in results:
-    data = []
     
-    for element in line:
-        if element.text.strip() != "":
-            data += [element.text.strip().replace("\xa0", " ")]
+    i=0
+
+    if pageCount == 0:
+        
+        
+        #For each row, store each first element (header) and an empty list
+        for t in tableElements[10]:
+            i+=1
+            name=t.text_content()
+            #print(i,name) \\test for printing column names
+            col.append([name,[]])
+        
+
+    for j in range(0,len(tableElements[11:])):
+        
+        #T is our j'th row
+        T=tableElements[11+j]
+        
+        #If row is not of size 10, the //tr data is not from our table 
+        if len(T)!=13:
+            continue
+        
+        #i is the index of our column
+        i=0
+        
+        #Iterate through each element of the row
+        for t in T.iterchildren():
+            data=t.text_content()
+            
+            #Check if row is empty
+            if i>0:
+            
+            #Convert any numerical value to integers
+                try:
+                    data=int(data)
+                except:
+                    pass
+                
+            #Append the data to the empty list of the i'th column
+            col[i][1].append(data)
+            
+            #Increment i for the next column
+            i+=1
     
-    while("" in data):
-        data.remove("")
+    #print([len(C) for (title,C) in col]) #test for printing how many number sections
+        
+    if nextPageSearchInputs[:3] != courseName[:3]:
+        break
+    else:
+        courseName = nextPageSearchInputs
+        pageCount += 1
     
-    sections += [data]
+    
+#setting up a dictionary for the dataFrame
+Dict = {title:column for [title,column] in col}
 
-sections = [x for x in sections if x]
-allSections = []
+#print([len(C) for [title,C] in col]) #test for printing how many number sections
 
-for row in range(len(sections)):
-    temp = []
-    if len(sections[row]) == 2 and sections[row-1][0][0:9].replace(" ", "") == "".join(courseName):
-        temp += sections[row-1]
-        for i in range(10):
-            if(sections[row + i] != []):
-                temp += sections[row + i]
-    allSections += [temp]
+courseNameString = [courseName[2]]
 
-allSections = [x for x in allSections if x]
+#creating dataframe and dropping extra classes
+df=pd.DataFrame(Dict)
+df = df[df["Class"].str.contains("|".join(courseNameString))]
+df.drop('', axis=1, inplace=True)
+df.drop('OpenSeats', axis=1, inplace=True)
 
-for x in allSections:
-    print(allSections)
+print(df)
+print("--- %s seconds ---" % (time.time() - start_time))
+
